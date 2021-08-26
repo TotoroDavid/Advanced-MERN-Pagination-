f7ef2a95817f60440e078887a3b03588bdba4b7c
TotoroDavid <davidtoro65@hotmail.com>
2021-08-26 16:43:41 -0500

    creating the frontend and connection with the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
-const App = () => {
+const App = ({ match }) => {
+  const pageNumber = match.params.pageNumber || 1
+
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [page, setPage] = useState(pageNumber)
+  const [pages, setPages] = useState(1)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch(`http://localhost:5000/api/v1/posts?page=${page}`)
+
+        const { data, page: totalPages } = await res.json()
+
+        setPages(totalPages)
+        setPosts(data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+        setError('Some error occurred')
+      }
+    }
+
+    fetchPosts()
+  }, [page])
+
   return (
-    <div>
-      hello world
+    <div className='App'>
+      {/* pagination component */}
+      {/* posts listing */}
+      {/* pagination component */}
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+//styles
+import './styles.css'
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Switch>
+      <Route exact path='/page/:pageNumber' component={App} />
+      <Route exact path='/' component={App} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 )
