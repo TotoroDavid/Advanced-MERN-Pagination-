2f3371153fa7fcb5b12a7a7701573a530b41d8a0
TotoroDavid <davidtoro65@hotmail.com>
2021-08-27 10:54:29 -0500

    finis the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// Components
+import Card from "./components/Card";
+import Pagination from "./components/Pagination";
 
 const App = ({ match }) => {
-  const pageNumber = match.params.pageNumber || 1
+  const pageNumber = match.params.pageNumber || 1;
 
-  const [posts, setPosts] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [page, setPage] = useState(pageNumber)
-  const [pages, setPages] = useState(1)
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [page, setPage] = useState(pageNumber);
+  const [pages, setPages] = useState(1);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
         const res = await fetch(`http://localhost:5000/api/v1/posts?page=${page}`)
 
-        const { data, page: totalPages } = await res.json()
+        const { data, pages: totalPages } = await res.json();
 
-        setPages(totalPages)
-        setPosts(data)
-        setLoading(false)
+        setPages(totalPages);
+        setPosts(data);
+        setLoading(false);
       } catch (error) {
-        console.log(error)
-        setLoading(false)
-        setError('Some error occurred')
+        console.log(error);
+        setLoading(false);
+        setError("Some error occurred");
       }
-    }
+    };
 
-    fetchPosts()
-  }, [page])
+    fetchPosts();
+  }, [page]);
 
   return (
-    <div className='App'>
-      {/* pagination component */}
-      {/* posts listing */}
-      {/* pagination component */}
+    <div className="app">
+      {loading ? (
+        <h3 className="loading-text">Loading...</h3>
+      ) : error ? (
+        <h3 className="error-text">{error}</h3>
+      ) : (
+        <>
+          <Pagination page={page} pages={pages} changePage={setPage} />
+          <div className="app__posts">
+            {posts.map((post) => (
+              <Card key={post._id} post={post} />
+            ))}
+          </div>
+          <Pagination page={page} pages={pages} changePage={setPage} />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
